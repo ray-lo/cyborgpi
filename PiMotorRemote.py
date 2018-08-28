0fd880c8c3a139a4faa83442e495f9756ec3c4f4
ray-lo <raylo@go.wustl.edu>
2018-08-28 18:52:46 -0500

    shit

--- PiMotorRemote.py
+++ PiMotorRemote.py
@@ -5,8 +5,9 @@ import pygame
 class RemoteCyborgPi(object):
 
     def __init__(self):
-        self.leftMotor = PiMotor.Motor("MOTOR1", 1)
-        self.rightMotor = PiMotor.Motor("MOTOR2", 2)
+        self.rightMotor = PiMotor.Motor("MOTOR1", 1)
+        self.leftMotor = PiMotor.Motor("MOTOR2", 1)
+
 
     def forward(self):
         self.leftMotor.forward(100)
@@ -15,7 +16,7 @@ class RemoteCyborgPi(object):
         self.leftMotor.reverse(100)
         self.rightMotor.reverse(100)
 
-    def rotateleft(self):
+    def rotateLeft(self):
         self.leftMotor.reverse(100)
         self.rightMotor.forward(100)
 
@@ -43,6 +44,10 @@ while 1:
     myPi.left()
     time.sleep(2)
     myPi.right()
+    time.sleep(2)
+    myPi.rotateLeft()
+    time.sleep(2)
+    myPi.rotateRight()
     time.sleep(2)
     myPi.forward()
     time.sleep(2)
