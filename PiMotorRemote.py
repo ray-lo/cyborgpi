feda677ac4d6cf24010d5e3fce369bdc2dfd9f74
Ray Lo <raylo@go.wustl.edu>
2018-09-20 21:56:52 +0000

    just in case

--- PiMotorRemote.py
+++ PiMotorRemote.py
@@ -1,3 +1,4 @@
+
 import PiMotor
 import time
 import curses
@@ -7,29 +8,29 @@ class RemoteCyborgPi(object):
     def __init__(self):
         self.rightMotor = PiMotor.Motor("MOTOR2", 1)
         self.leftMotor = PiMotor.Motor("MOTOR1", 2)
-
+	self.speed = 100
 
     def forward(self):
-        self.leftMotor.forward(100)
-        self.rightMotor.forward(100)
+        self.leftMotor.forward(self.speed)
+        self.rightMotor.forward(self.speed)
     def reverse(self):
-        self.leftMotor.reverse(100)
-        self.rightMotor.reverse(100)
+        self.leftMotor.reverse(self.speed)
+        self.rightMotor.reverse(self.speed)
 
     def rotateLeft(self):
-        self.leftMotor.reverse(100)
-        self.rightMotor.forward(100)
+        self.leftMotor.reverse(self.speed)
+        self.rightMotor.forward(self.speed)
 
     def rotateRight(self):
-        self.leftMotor.forward(100)
-        self.rightMotor.reverse(100)
+        self.leftMotor.forward(self.speed)
+        self.rightMotor.reverse(self.speed)
 
     def left(self):
         self.leftMotor.stop()
-        self.rightMotor.forward(100)
+        self.rightMotor.forward(self.speed)
 
     def right(self):
-        self.leftMotor.forward(100)
+        self.leftMotor.forward(self.speed)
         self.rightMotor.stop()
 
     def stop(self):
