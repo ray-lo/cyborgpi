f462d1f91ee4a4ab675b203182c569873f85dd74
Ray Lo <raylo@go.wustl.edu>
2018-11-01 21:48:00 +0000

    finally working

--- singleLineFollower.py
+++ singleLineFollower.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as IO
 #IO.setwarnings(False)
+sensor = 25
 IO.setmode (IO.BCM)
-IO.setup(2,IO.IN)
-
+IO.setup(sensor, IO.IN, pull_up_down=IO.PUD_UP)
 while (True):
-    print IO.input(2)
+   # if IO.input(sensor) != 0:
+	#print "Hit"
+    print IO.input(sensor)
