5b093a301da89e42c287cdda975e0a02890d7a76
Ray Lo <raylo@go.wustl.edu>
2018-11-09 22:04:47 +0000

    all 5 sensors confirmed to be working

--- LineSensingCyborgPi.py
+++ LineSensingCyborgPi.py
@@ -2,20 +2,24 @@ import RPi.GPIO as IO
 
 ##the numbering of the sensors aren't in sequence mainly due to which GPIO means are physically closest/convinient/neat to the sensors themselves
 LLSensor = 24 #1st sensor from the left
-LSensor = 21 #2nd sensor from the left
-CSensr = 23 #sensor in the middle
-RSensor = 20 #2nd sensor from the right
-RRSensor = 26 #1st sensor from the right
+LSensor = 20 #2nd sensor from the left
+CSensor = 23 #sensor in the middle
+RSensor = 21 #2nd sensor from the right
+RRSensor = 7 #1st sensor from the right
 
 
 IO.setmode (IO.BCM)
-IO.setup(LLsensor, IO.IN, pull_up_down=IO.PUD_UP)
-IO.setup(Lsensor, IO.IN, pull_up_down=IO.PUD_UP)
-IO.setup(Csensor, IO.IN, pull_up_down=IO.PUD_UP)
-IO.setup(Rsensor, IO.IN, pull_up_down=IO.PUD_UP)
-IO.setup(RRsensor, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(LLSensor, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(LSensor, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(CSensor, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(RSensor, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(RRSensor, IO.IN, pull_up_down=IO.PUD_UP)
 
+inputs = [0,0,0,0,0]
 while (True):
-   # if IO.input(sensor) != 0:
-	#print "Hit"
-    print IO.input(sensor)
+	inputs[0] = IO.input(LLSensor)
+	inputs[1] = IO.input(LSensor)
+	inputs[2] = IO.input(CSensor)
+	inputs[3] = IO.input(RSensor)
+	inputs[4] = IO.input(RRSensor)
+	print inputs
